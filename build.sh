79bc91221ea69a2bd4c676541383ffff5593ba3d
Yan <me@octonezd.me>
2026-01-07 16:40:20 +0300

    Show notice when installing chrony for first time

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ cp build_inner.sh chrony-4.7
 docker run --rm -it -v $(pwd)/chrony-4.7:/src -u $(id -u):$(id -g) -w /src ghcr.io/octonezd/meego_docker:latest bash build_inner.sh
 cp chrony-4.7/chronyd data/usr/bin/chronyd
 patchelf --set-interpreter /lib/ld-linux.so.3 data/usr/bin/chronyd
+cp chrony.conf data/etc/init/chrony.conf
+cp chrony.conf.sample data/etc/chrony.conf.sample
